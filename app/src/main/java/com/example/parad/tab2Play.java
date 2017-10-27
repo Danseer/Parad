df4dbd286fa281083da1fa476fdcf561515ed96a
Konstantin <mr.ks83@mail.ru>
2017-10-27 13:52:45 +0300

    first

---
+++ app/src/main/java/com/example/parad/tab2Play.java
@@ -0,0 +1,20 @@
+package com.example.parad;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Константин on 26.10.2017.
+ */
+
+public class tab2Play extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2play, container, false);
+        return rootView;
+    }
+}
